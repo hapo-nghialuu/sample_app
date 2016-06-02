d3db0488715df454fe167066ada9d1b16f9039c0
toxingau <toxingau@gmail.com>
2016-06-02 15:14:47 +0700

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,7 +5,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name: "", email: "user@invalid", password: "foo", password_confirmation: "bar" }
+      post users_path, user: { name: "", birthday: "06/03/2017", email: "user@invalid", password: "foo", password_confirmation: "bar" }
     end
     assert_template 'users/new'
   end
@@ -14,6 +14,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: { name:  "Example User",
+                                            birthday: "02/21/1994",
                                             email: "user@example.com",
                                             password:              "password",
                                             password_confirmation: "password" }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
  
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com",
+    @user = User.new(name: "Example User", birthday: "06/02/2016", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
 
@@ -66,4 +66,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
