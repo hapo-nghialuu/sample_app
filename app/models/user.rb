b5f0aa546e25c66f518feac574f0d0c5b788e489
toxingau <toxingau@gmail.com>
2016-06-02 09:26:59 +0700

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,19 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :birthday, presence: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  validate :checkbirthday
+
+  private 
+  def checkbirthday
+  	if !self.birthday.nil?  	
+  		if self.birthday > Time.now
+    		errors.add(:birtday, "cannot be in the future")
+  		elsif self.birthday < Time.now - 125.years
+    		errors.add(:birthday, "cannot be over 125 years ago")
+  		end
+  	end
+   end
 end
